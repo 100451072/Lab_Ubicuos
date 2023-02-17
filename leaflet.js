9afbd371a3277c3e67441b38f7cf3dee291c09c4
100451072 <100451072@alumnos.uc3m.es>
2023-02-17 18:43:26 +0000

    funciona el poner y quitar circulos

--- leaflet.js
+++ leaflet.js
@@ -1,30 +1,45 @@
-function initialize() {
+let CIRCLE = [];
+
+(function initialize() {
+    // Init varibles
     map = L.map('map').setView([51.505, -0.09], 0);
+
     // Establecemos el mapa
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    // Pop-up para mostrar info en el mapa
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('Inemuri')
-        .openPopup();
-    
-    // Pasamos la funcion onMapClick, para ser ejecutada en el evento "click"
-    map.on('click', onMapClick);
+
+    // Para interactuar con el mapa, con diferentes eventos
+    // map.once() ==> Para ejecutar una vez
+    map.on('click', onMapClickCircle);
+    map.on('keydown', onMapKeyDown)
     // Establecer la localizacion del mapa 
     map.locate({setView: true, maxZoom: 8});    
-}
+})()
 
 // Captura de evento: click
-function onMapClick(e) {
+function onMapClickCircle(e) {
     // Creacion de circulo
-    var circle = L.circle(e.latlng, {
+    console.log(CIRCLE)
+    CIRCLE.push(L.circle(e.latlng, {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 10000
-    }).addTo(map)
+        radius: 1000
+    }).addTo(map))
 }
 
-// MAIN
-initialize();
+function onMapClickMarker(e) {
+    // Pop-up para mostrar info en el mapa
+    marker = L.marker(e.latlng).addTo(map)
+        .bindPopup('Destino')
+        .openPopup();
+}
+
+function onMapKeyDown(e) {
+    if (event.key === "q") {
+        // Eliminacion de circulo
+        CIRCLE.pop().remove();
+        console.log(CIRCLE);
+    }
+}
